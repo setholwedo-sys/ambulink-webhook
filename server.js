a3f7f735a93bd2161c1f058a6877160335822d78
setholwedo-sys <setholwedo@gmail.com>
2026-07-25 02:04:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -160,5 +160,4 @@ app.post('/api/webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
